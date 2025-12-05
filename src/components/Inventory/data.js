125ad68f0c7aa80d934d1c6cdeb3d6118a0ded56
Finder359 <GitHub>
2025-12-05 21:12:39 +0800

    v2.1增加了部分车辆信息

--- src/components/Inventory/data.js
+++ src/components/Inventory/data.js
@@ -15,7 +15,105 @@ export const cars = [
       "星空灰车漆",
       "19 英寸新星轮毂",
       "深色高级内饰",
-      "基础辅助驾驶"
+      "基础辅助驾驶",
+      "转向信号拨杆"
     ]
-  }
+  },
+  {
+    id: 2,
+    model: "Model 3",
+    trim: "长续航后轮驱动版",
+    price: 259500,
+    range: 830,
+    topSpeed: 200,
+    acceleration: 5.2,
+    images: [
+      "/images/Inventory-images/model3-long-range-rear-wheel/1.jpg",
+        "/images/Inventory-images/model3-long-range-rear-wheel/2.jpg"
+    ],
+    features: [
+      "珍珠白（多涂层）车漆",
+      "18 英寸光子轮毂",
+      "深色高级内饰（后轮驱动版）",
+      "基础辅助驾驶",
+      "转向信号拨杆"
+    ]
+  },
+  {
+    id: 3,
+    model: "Model 3",
+    trim: "长续航全轮驱动版",
+    price: 289500,
+    range: 753,
+    topSpeed: 200,
+    acceleration: 3.8,
+    images: [
+      "/images/Inventory-images/model3-long-range-all-wheel/1.jpg",
+        "/images/Inventory-images/model3-long-range-all-wheel/2.jpg"
+    ],
+    features: [
+      "烈焰红车漆",
+      "18 英寸光子轮毂",
+      "深色高级内饰",
+      "基础辅助驾驶",
+      "转向信号拨杆",
+      "加速提升"
+    ]
+  },
+  {
+    id: 4,
+    model: "Model S",
+    trim: "全轮驱动版",
+    price: 855900,
+    range: 715,
+    topSpeed: 240,
+    acceleration: 3.2,
+    images: [
+      "/images/Inventory-images/models-all-wheel/1.jpg",
+        "/images/Inventory-images/models-all-wheel/2.jpg",
+      "/images/Inventory-images/models-all-wheel/3.jpg",
+      "/images/Inventory-images/models-all-wheel/4.jpg",
+      "/images/Inventory-images/models-all-wheel/5.jpg"
+
+    ],
+    features: [
+      "珍珠白（多涂层）车漆",
+      "19 英寸旋动磁力轮毂",
+      "纯黑色高级内饰（黑色座椅，黑檀木饰板）",
+      "基础辅助驾驶",
+      "标准方向盘",
+      "舒适悬挂",
+      "更静谧的驾乘体验",
+      "动态氛围灯",
+      "前保险杠摄像头"
+    ]
+  },
+  {
+    id: 5,
+    model: "Model S",
+    trim: "全轮驱动版",
+    price: 855900,
+    range: 715,
+    topSpeed: 240,
+    acceleration: 3.2,
+    images: [
+      "/images/Inventory-images/models-all-wheel/1.jpg",
+        "/images/Inventory-images/models-all-wheel/2.jpg",
+      "/images/Inventory-images/models-all-wheel/3.jpg",
+      "/images/Inventory-images/models-all-wheel/4.jpg",
+      "/images/Inventory-images/models-all-wheel/5.jpg"
+
+    ],
+    features: [
+      "珍珠白（多涂层）车漆",
+      "19 英寸旋动磁力轮毂",
+      "纯黑色高级内饰（黑色座椅，黑檀木饰板）",
+      "基础辅助驾驶",
+      "标准方向盘",
+      "舒适悬挂",
+      "更静谧的驾乘体验",
+      "动态氛围灯",
+      "前保险杠摄像头"
+    ]
+  },
 ];
