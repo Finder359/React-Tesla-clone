f9ad4aec1d70796c08afb70129eae8d040e6666d
Finder359 <GitHub>
2025-12-06 10:38:35 +0800

    v2.2车辆信息补全

--- src/components/Inventory/data.js
+++ src/components/Inventory/data.js
@@ -97,17 +97,74 @@ export const cars = [
     topSpeed: 240,
     acceleration: 3.2,
     images: [
-      "/images/Inventory-images/models-all-wheel/1.jpg",
-        "/images/Inventory-images/models-all-wheel/2.jpg",
-      "/images/Inventory-images/models-all-wheel/3.jpg",
-      "/images/Inventory-images/models-all-wheel/4.jpg",
-      "/images/Inventory-images/models-all-wheel/5.jpg"
+      "/images/Inventory-images/models-all-wheel-black/1.jpg",
+        "/images/Inventory-images/models-all-wheel-black/2.jpg",
+      "/images/Inventory-images/models-all-wheel-black/3.jpg",
+      "/images/Inventory-images/models-all-wheel-black/4.jpg",
+      "/images/Inventory-images/models-all-wheel-black/5.jpg"
+
+    ],
+    features: [
+      "星钻黑车漆",
+      "19 英寸旋动磁力轮毂",
+      "纯黑色高级内饰（黑色座椅，黑檀木饰板）",
+      "基础辅助驾驶",
+      "标准方向盘",
+      "舒适悬挂",
+      "更静谧的驾乘体验",
+      "动态氛围灯",
+      "前保险杠摄像头"
+    ]
+  },
+  {
+    id: 6,
+    model: "Model Y",
+    trim: "长续航全轮驱动版",
+    price: 325500,
+    range: 750,
+    topSpeed: 201,
+    acceleration: 4.3,
+    images: [
+      "/images/Inventory-images/modely-long-range-all-wheel/1.jpg",
+        "/images/Inventory-images/modely-long-range-all-wheel/2.jpg",
+      "/images/Inventory-images/modely-long-range-all-wheel/3.jpg",
+      "/images/Inventory-images/modely-long-range-all-wheel/4.jpg",
+      "/images/Inventory-images/modely-long-range-all-wheel/5.jpg",
+      "/images/Inventory-images/modely-long-range-all-wheel/6.jpg"
+
+    ],
+    features: [
+      "烈焰红车漆",
+      "19 英寸交互风暴轮毂",
+      "深色高级内饰（长续航全轮驱动版）",
+      "基础辅助驾驶",
+      "五座版",
+      "加速提升",
+      
+    ]
+  },
+  {
+    id: 7,
+    model: "Model X",
+    trim: "全轮驱动版",
+    price: 912900,
+    range: 700,
+    topSpeed: 240,
+    acceleration: 3.9,
+    images: [
+      "/images/Inventory-images/modelx-all-wheel/1.jpg",
+        "/images/Inventory-images/modelx-all-wheel/2.jpg",
+      "/images/Inventory-images/modelx-all-wheel/3.jpg",
+      "/images/Inventory-images/modelx-all-wheel/4.jpg",
+      "/images/Inventory-images/modelx-all-wheel/5.jpg"
 
     ],
     features: [
       "珍珠白（多涂层）车漆",
-      "19 英寸旋动磁力轮毂",
-      "纯黑色高级内饰（黑色座椅，黑檀木饰板）",
+      "20 英寸近日点轮毂",
+      "黑白高级内饰（白色座椅，胡桃木饰板）",
+      "五座座椅布局",
+      "牵引套件",
       "基础辅助驾驶",
       "标准方向盘",
       "舒适悬挂",
